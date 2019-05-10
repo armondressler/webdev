64eff8be608100a218eb1e6b80c1bf8dfc17b35b
armon.dressler@gmail.com <armon.dressler@gmail.com>
2019-05-10 20:22:42 +0200

    removed js textbox/dropdown chimera

--- viewPatient.php
+++ viewPatient.php
@@ -413,21 +413,11 @@ echo '<br>
       <div>
       <form action="/logout.php" method="get">
       <button type="submit">Logout as '.$_SESSION['user'];
-echo '
-</button>';
+echo '</button>
+      </form>
+      </div>';
 
 ?>
-
-<form>
-  <input type="text" name="myText" value="Norway" selectBoxOptions="Canada;Denmark;Finland;Germany;Mexico;Norway;Sweden;United Kingdom;United States">
-	<input type="text" name="myText2" value="" selectBoxOptions="Amy;Andrew;Carol;Jennifer;Jim;Tim;Tommy;Vince">
-</form>
-
-<script type="text/javascript">
-createEditableSelect(document.forms[0].myText);
-createEditableSelect(document.forms[0].myText2);
-</script>
-
 </body>
 </html>
 
